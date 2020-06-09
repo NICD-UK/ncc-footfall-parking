7e7b432e4bc4d9554c705ddba2dd342f3653413e
Mark Turner <nmdt3@18DCompL027.local>
2020-06-09 15:10:09 +0100

    initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,23 @@
+import '../styles/index.scss';
+import 'bootstrap';
+import 'leaflet';
+import $ from 'jquery';
+
+const map = L.map('map', { zoomControl: false }).setView([54.9759, -1.6128], 16);
+
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.control.zoom({
+    position:'topright'
+}).addTo(map);
+
+// L.marker([51.5, -0.09]).addTo(map)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//     .openPopup();
+
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
