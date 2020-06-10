e993251f580a73f0b82efded70d9ad39ba6512f6
Mark Turner <nmdt3@18DCompL027.local>
2020-06-10 11:59:45 +0100

    Added all of the known car parks

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,8 +2,9 @@ import '../styles/index.scss';
 import 'bootstrap';
 import 'leaflet';
 import $ from 'jquery';
+import carparks from '../../public/assets/data/carparks';
 
-const map = L.map('map', { zoomControl: false }).setView([54.9759, -1.6128], 16);
+const map = L.map('map', { zoomControl: false }).setView([54.9759, -1.6128], 14);
 
 L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -13,12 +14,29 @@ L.control.zoom({
     position:'topright'
 }).addTo(map);
 
-// L.marker([51.5, -0.09]).addTo(map)
-//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-//     .openPopup();
+const states = ['quiet', 'average', 'busy'];
+
+const markers = [];
+
+states.forEach(function(state){
+    markers[state] = L.divIcon({
+        html: '<i class="feather icon-map-pin ' + state + '"></i>',
+        iconSize: [30, 30],
+        className: 'car-park-marker'
+    });
+});
+
+carparks.forEach(function(carpark){
+    if(carpark.location.length === 2) {
+        L.marker(carpark.location, { icon: markers.busy }).addTo(map)
+        .bindPopup('There are only ' + carpark.capacity + ' spaces remaing at ' + carpark.name + '.');
+    }
+});
 
 $("#menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
     $("#menu-toggle").toggleClass("is-active");
 });
+
+//https://nccfootfallparking.blob.core.windows.net/api-data/latest.json
